205faf68bab167e7da4ab31bf93b2ad3d2a98a14
Ajay Paul Indana <Ajay_Indana@s5a.com>
2018-05-17 11:59:01 -0700

    Update Volume.js

--- App/Components/Volume.js
+++ App/Components/Volume.js
@@ -5,8 +5,8 @@ import styles from './Styles/VolumeStyle'
 
 export default class Volume extends Component {
   static propTypes = {
-    text: PropTypes.number,
-    day: PropTypes.number
+    VolPer: PropTypes.number,
+    VolChange: PropTypes.string
   }
   // // Prop type warnings
   // static propTypes = {
@@ -20,24 +20,17 @@ export default class Volume extends Component {
   // }
 
   render () {
-    if(this.props.day>75 && this.props.text>100){
+    if(this.props.VolChange=="up"){
       return (
         <View style={styles.container} backgroundColor='green'>
-          <Text style={styles.textbox}>{this.props.text}</Text>
-        </View>
-      )
-    }
-    else if(this.props.day<25 && this.props.text>100){
-      return (
-        <View style={styles.container} backgroundColor='red'>
-          <Text style={styles.textbox}>{this.props.text}</Text>
+          <Text style={styles.textbox}>{this.props.VolPer}</Text>
         </View>
       )
     }
     else {
       return (
-        <View style={styles.container} backgroundColor='grey'>
-          <Text style={styles.textbox}>{this.props.text}</Text>
+        <View style={styles.container} backgroundColor='red'>
+          <Text style={styles.textbox}>{this.props.VolPer}</Text>
         </View>
       )
     }
